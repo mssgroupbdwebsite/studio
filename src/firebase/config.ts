78d11ca4b0c645fca1ee89f5a831a11dba9873eb
mssgroupbdwebsite <mssgroupbdwebsite@gmail.com>
2025-12-20 19:13:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2779115415-2fdb9",
+  "appId": "1:604004472088:web:23ea2297f305ab31047a6d",
+  "apiKey": "AIzaSyA0uKuwTfKVwNnyPyv_ul1fnU9XiqBjBo8",
+  "authDomain": "studio-2779115415-2fdb9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "604004472088"
+};
