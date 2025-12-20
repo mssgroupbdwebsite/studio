04442c690d98f1c74c1d6bf85c587e369cbae0c8
mssgroupbdwebsite <mssgroupbdwebsite@gmail.com>
2025-12-20 19:07:37 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,12 +3,12 @@ import {initializeApp, getApps, getApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA0uKuwTfKVwNnyPyv_ul1fnU9XiqBjBo8",
+  authDomain: "studio-2779115415-2fdb9.firebaseapp.com",
+  projectId: "studio-2779115415-2fdb9",
+  storageBucket: "studio-2779115415-2fdb9.appspot.com",
+  messagingSenderId: "604004472088",
+  appId: "1:604004472088:web:23ea2297f305ab31047a6d"
 };
 
 // Initialize Firebase
