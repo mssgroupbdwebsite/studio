998857814b5e93b1e945240d8083fe6442d4707f
mssgroupbdwebsite <mssgroupbdwebsite@gmail.com>
2025-12-20 20:53:08 +0000

    Set up a Firebase backend

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,23 +1,43 @@
-
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword as firebaseSignIn, createUserWithEmailAndPassword as firebaseSignUp, signOut as firebaseSignOut } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
 
-let firebaseApp: FirebaseApp;
-if (!getApps().length) {
-  firebaseApp = initializeApp(firebaseConfig);
-} else {
-  firebaseApp = getApp();
+// IMPORTANT: DO NOT MODIFY THIS FUNCTION
+export function initializeFirebase() {
+  if (!getApps().length) {
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
+    }
+
+    return getSdks(firebaseApp);
+  }
+
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
 }
 
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
-
-export function initializeFirebase() {
-  return { firebaseApp, auth, firestore };
+export function getSdks(firebaseApp: FirebaseApp) {
+  return {
+    firebaseApp,
+    auth: getAuth(firebaseApp),
+    firestore: getFirestore(firebaseApp)
+  };
 }
 
 export * from './provider';
@@ -25,21 +45,6 @@ export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
+export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
-
-// Auth related client actions
-export async function signInWithEmailAndPassword(email: string, password: string) {
-  const { auth } = initializeFirebase();
-  return firebaseSignIn(auth, email, password);
-}
-
-export async function createUserWithEmailAndPassword(email: string, password: string) {
-  const { auth } = initializeFirebase();
-  return firebaseSignUp(auth, email, password);
-}
-
-export async function signOut() {
-  const { auth } = initializeFirebase();
-  await firebaseSignOut(auth);
-}
