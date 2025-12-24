0ba14113831f2087cf94a2d0e271a6a5f76f428d
mssgroupbdwebsite <mssgroupbdwebsite@gmail.com>
2025-12-24 16:38:21 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // This is required to allow the Next.js dev server to be accessed from the Studio preview.
-  allowedDevOrigins: [
-    '*.cluster-yylgzpipxrar4v4a72liastuqy.cloudworkstations.dev',
-  ],
   typescript: {
     ignoreBuildErrors: true,
   },
