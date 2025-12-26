f0ef82fb706078402d7a0126287f189251b3153c
mssgroupbdwebsite <mssgroupbdwebsite@gmail.com>
2025-12-26 19:22:15 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-2779115415-2fdb9",
-  "appId": "1:604004472088:web:23ea2297f305ab31047a6d",
-  "apiKey": "AIzaSyA0uKuwTfKVwNnyPyv_ul1fnU9XiqBjBo8",
-  "authDomain": "studio-2779115415-2fdb9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "604004472088"
+  apiKey: "AIzaSyAe9mAOMGO73g7NTq7RQ4iCtbazfLPUv4I",
+  authDomain: "studio-2779115415-2fdb9.firebaseapp.com",
+  databaseURL: "https://studio-2779115415-2fdb9-default-rtdb.firebaseio.com",
+  projectId: "studio-2779115415-2fdb9",
+  storageBucket: "studio-2779115415-2fdb9.appspot.com",
+  messagingSenderId: "604004472088",
+  appId: "1:604004472088:web:aba57e871e648633047a6d"
 };
