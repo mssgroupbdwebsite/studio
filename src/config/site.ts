ea4e2db7723443c82c3c7725c61e9d0cd5fef43a
mssgroupbdwebsite <mssgroupbdwebsite@gmail.com>
2025-12-29 21:54:35 +0000

    i mean change the name from other to an another name i didnt mean to nam

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,7 +9,7 @@ export const siteConfig = {
     { title: "Home", href: "/" },
     { title: "Products", href: "/products" },
     { title: "Company", href: "/about" },
-    { title: "Other", href: "/gallery" },
+    { title: "Our Team", href: "/gallery" },
     { title: "Quality", href: "/quality" },
     { title: "Sustainability", href: "/sustainability" },
     { title: "Blog", href: "/blog" },
